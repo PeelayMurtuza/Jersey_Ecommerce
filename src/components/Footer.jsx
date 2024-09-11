f0f90c9b8c7d275abed701a2589ca49ef04f9364
PeelayMurtuza <peelaymurtuza@gmail.com>
2024-09-11 20:17:26 +0530

    CopyRight footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-gray-900 text-white py-4 w-full ">
       <div className="container text-center">
         <p className="text-sm">
-          &copy; {new Date().getFullYear()} Peelay Murtuza Afsar . All rights reserved.
+          &copy; {new Date().getFullYear()} Peelay Murtuza Afsar & Khot Saami . All rights reserved.
         </p>
       </div>
     </footer>
